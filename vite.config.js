ceb10cce86261f58b336c97a06c01e2f7b7ea63d
reddy <reddymaruri9@gmail.com>
2024-06-21 09:55:17 +0530

    modified

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "src",
-  base: "./", // Set the correct base path
+  base: "./src", // Set the correct base path
 });
